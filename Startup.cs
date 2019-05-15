d5aea6b5c67a736df99c95d2a7d527dc3f707714
Vinicius Camargo <vinicius.camargo@uppertools.com.br>
2019-05-15 08:43:48 -0300

    Redirecionamento para swagger ao abrir a aplicação

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
@@ -61,6 +62,10 @@ namespace ApiMySql
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Projeto Quadra API - .NET Core");
             });
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
 
             app.UseMvc();
         }
